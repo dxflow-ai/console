5fab42a24638202ba6c0ee2ee29530de4560a529
Amir Reza Dalir <dalirnet@gmail.com>
2026-06-14 18:45:03 +0330

    feat: console index and routing

---
+++ app/pages/console/index.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script lang="ts" setup>
+definePageMeta({
+    name: "console",
+    redirect: "/console/engine/overview/",
+});
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script lang="ts" setup>
+definePageMeta({
+    name: "index",
+    redirect: "/console/engine/overview/",
+});
+</script>
