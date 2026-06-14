6aecbcee6f3b911b000c72360b5226a30f6494ed
Amir Reza Dalir <dalirnet@gmail.com>
2026-06-14 18:44:22 +0330

    feat: app shell and error page

---
+++ app/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <UiApp
+        :tooltip="{
+            delayDuration: 250,
+        }"
+        :toaster="{
+            position: 'bottom-center',
+            duration: 7500,
+            expand: false,
+            progress: false,
+        }"
+    >
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </UiApp>
+</template>
---
+++ app/error.vue
@@ -0,0 +1,60 @@
+<template>
+    <UiMain>
+        <UiError
+            :redirect="redirect"
+            :error="{
+                statusCode,
+                statusMessage,
+            }"
+            :clear="{
+                size: 'sm',
+                color: 'zodiac',
+                variant: 'soft',
+                label: 'Back',
+            }"
+        >
+            <template #message>
+                <span>{{ message }}</span>
+            </template>
+        </UiError>
+    </UiMain>
+</template>
+
+<script setup lang="ts">
+type ErrorProps = {
+    statusCode?: number;
+    message?: string;
+};
+
+const props = defineProps({
+    error: {
+        type: Object as PropType<ErrorProps>,
+        required: true,
+    },
+});
+
+const route = useRoute();
+
+const statusCode = computed(() => {
+    return props.error?.statusCode || 0;
+});
+const statusMessage = computed(() => {
+    return httpStatus[statusCode.value] || "Unknown";
+});
+
+const message = computed(() => {
+    return props.error?.message || "An error occurred";
+});
+
+const redirect = computed(() => {
+    if (route.path.startsWith("/docs")) {
+        return "/docs";
+    }
+
+    if (route.path.startsWith("/console")) {
+        return "/console";
+    }
+
+    return "/";
+});
+</script>
